8884595888f89abcee2d6425041d084d8635b35c
dishantsethi <dishantsethi14@gmail.com>
2023-09-13 11:14:04 +0530

    bug fix, add poetry support

---
+++ {{ cookiecutter.main_module }}/settings/testing.py
@@ -0,0 +1,11 @@
+# Do this here, so that .env get loaded while running `pytest` from shell
+# Third Party Stuff
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+from .development import *  # noqa F405
+
+MEDIA_ROOT = "/tmp"
+
+SECRET_KEY = "top-scret!"
